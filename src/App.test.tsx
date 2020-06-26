aeeda2d0d7a3319d7630eb81804d95d521f3731d
Jakob J. Kolb <kolb@hardfork.io>
2020-06-26 11:42:53 +0200

    Add enzyme and chai for testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
+import { assert } from 'chai'
+import { mount } from 'enzyme'
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const wrapper = mount(<App />)
+  assert.equal(
+    wrapper.find('.App-link').text(),
+    'Learn React',
+    'Link should say "Learn React"',
+  )
+  assert.equal(wrapper.find('.App-link').props().href, 'https://reactjs.org')
 })
